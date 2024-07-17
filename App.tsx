26c71c2729c2d57d3a88dc6c800e79a830f1a5f7
김세원 <i3brooooo@gmail.com>
2024-07-17 14:19:55 +0900

    test : Husky 깃 커밋 시 eslint, prettier 반영되도록 테스트중

--- App.tsx
+++ App.tsx
@@ -12,6 +12,8 @@ import { useColorScheme } from 'nativewind';
 function App(): React.JSX.Element {
   const { colorScheme, toggleColorScheme } = useColorScheme();
 
+  console.log('hello');
+
   return (
     <SafeAreaView className="flex-1 bg-white dark:bg-black">
       <StatusBar barStyle={colorScheme === 'light' ? 'dark-content' : 'light-content'} />

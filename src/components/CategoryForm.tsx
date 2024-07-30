a469e976b17c868575fa1f2b76519b508b6bb9cd
Sewon Kim <i3brooooo@gmail.com>
2024-07-30 22:58:53 +0900

    feat : CategoryForm.tsx 작성 모달 완료 feat : FadeInOut.tsx 컴포넌트 추가

--- src/components/CategoryForm.tsx
+++ src/components/CategoryForm.tsx
@@ -2,6 +2,9 @@ import React, { useRef, useState } from 'react';
 import { Alert, Modal, Pressable, Text, TextInput, View } from 'react-native';
 import { X } from 'lucide-react-native';
 
+import FadeInFadeOut from '@/components/Animation/FadeInOut.tsx';
+import { Button } from '@/components/Button.tsx';
+import ColorPicker from '@/components/ColorPicker.tsx';
 import useMatrixStore from '@/stores/matrix.ts';
 
 const placeholderData = ['여행', '요리', '쇼핑', '회사', '공부'];
@@ -13,6 +16,10 @@ const CategoryForm = ({ open, onClose }: { open: boolean; onClose: () => void })
 
   const [categoryValue, setCategoryValue] = useState<string>('');
 
+  const [isOpenColorPicker, setIsOpenColorPicker] = useState<boolean>(false);
+
+  const [isSelectedColor, setIsSelectedColor] = useState<string>('#d9d9d9');
+
   const { addMatrix } = useMatrixStore();
 
   const onAddCategory = () => {
@@ -23,22 +30,29 @@ const CategoryForm = ({ open, onClose }: { open: boolean; onClose: () => void })
       // 카테고리 zustand store에 추가
       addMatrix({
         category: categoryValue.trim(),
-        categoryBackgroundColor: '#d9d9d9',
+        categoryBackgroundColor: isSelectedColor,
       });
-      onClose();
+
+      // 상태 초기화 및 모달 닫기
+      onCloseModal();
     }
   };
 
+  // 카테고리 입력
+  const onChangeCategoryText = (text: string) => {
+    // const replaceText = text.replace(/(\s*)/g, '');
+    setCategoryValue(text);
+  };
+
+  // 상태 초기화 및 모달 닫기
+  const onCloseModal = () => {
+    setCategoryValue('');
+    setIsSelectedColor('#d9d9d9');
+    onClose();
+  };
+
   return (
-    <Modal
-      animationType="slide"
-      visible={open}
-      onRequestClose={() => {
-        setCategoryValue('');
-        onClose();
-      }}
-      presentationStyle="pageSheet"
-    >
+    <Modal animationType="slide" visible={open} onRequestClose={onCloseModal} presentationStyle="pageSheet">
       <View style={{ flex: 1, backgroundColor: 'white' }}>
         {/* 모달 카테고리 닫기 버튼 */}
         <View className="flex-[0.1] flex-row items-center justify-between px-2 py-2">
@@ -46,7 +60,7 @@ const CategoryForm = ({ open, onClose }: { open: boolean; onClose: () => void })
             <Text className="text-lg font-semibold">완료</Text>
           </Pressable>
 
-          <Pressable className="h-12 w-12 items-center justify-center" onPress={() => onClose()}>
+          <Pressable className="h-12 w-8 items-center justify-center" onPress={onCloseModal}>
             <X size={27} color="black" />
           </Pressable>
         </View>
@@ -58,15 +72,14 @@ const CategoryForm = ({ open, onClose }: { open: boolean; onClose: () => void })
               ref={inputRef}
               className="w-full text-center text-3xl font-bold"
               value={categoryValue}
-              onChangeText={(text) => {
-                setCategoryValue(text.replace(/(\s*)/g, ''));
-              }}
+              onChangeText={onChangeCategoryText}
               placeholder={placeholderData[0]}
               placeholderTextColor="#999999"
               returnKeyType="done"
               autoFocus
               onSubmitEditing={onAddCategory}
             />
+            {/* placeholder */}
             {!categoryValue &&
               placeholderData
                 .filter((item, index) => index > 0)
@@ -77,9 +90,25 @@ const CategoryForm = ({ open, onClose }: { open: boolean; onClose: () => void })
                     </Text>
                   );
                 })}
+            <FadeInFadeOut style={{ marginTop: 20 }} fadeIn={categoryValue.length > 0}>
+              <Button variant="outline" size="icon" onPress={() => setIsOpenColorPicker(true)}>
+                <View style={{ width: 24, height: 24, borderRadius: 20, backgroundColor: isSelectedColor }} />
+              </Button>
+            </FadeInFadeOut>
           </View>
         </View>
       </View>
+
+      <ColorPicker
+        open={isOpenColorPicker}
+        onClose={() => {
+          setIsOpenColorPicker(false);
+        }}
+        onSelect={(color) => {
+          setIsOpenColorPicker(false);
+          setIsSelectedColor(color);
+        }}
+      />
     </Modal>
   );
 };
